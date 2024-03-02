1d2180e09763334efd5733ba78f1a2336771661b
anderskambenders <a.zetsmanis@mail.ru>
2024-03-02 17:17:16 +0300

    feat: add change, delete profile

--- src/routes/graphql/schemas/profile/mutation.ts
+++ src/routes/graphql/schemas/profile/mutation.ts
@@ -14,6 +14,15 @@ export const createProfileInputType = new GraphQLInputObjectType({
   }),
 });
 
+export const changeProfileInputType = new GraphQLInputObjectType({
+  name: 'ChangeProfileInput',
+  fields: () => ({
+    isMale: { type: GraphQLBoolean },
+    yearOfBirth: { type: GraphQLInt },
+    memberTypeId: { type: memberTypeId },
+  }),
+});
+
 export const profileMutations = {
   createProfile: {
     type: profileType,
@@ -31,6 +40,42 @@ export const profileMutations = {
       },
     ) => {
       const profile = await context.prismaClient.profile.create({ data: args.dto });
+      return profile;
+    },
+  },
+  deleteProfile: {
+    type: GraphQLBoolean,
+    args: { id: { type: UUIDType } },
+    resolve: async (_parent: unknown, args: { id: string }, context: {
+      prismaClient: PrismaClient;
+    }) => {
+      try {
+        await context.prismaClient.profile.delete({ where: { id: args.id } });
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
+  },
+  changeProfile: {
+    type: profileType,
+    args: { id: { type: UUIDType }, dto: { type: changeProfileInputType } },
+    resolve: async (
+      _parent: unknown,
+      args: { id: string; dto: {
+        isMale: boolean;
+        yearOfBirth: number;
+        userId: string;
+        memberTypeId: string;
+      } },
+      context: {
+        prismaClient: PrismaClient;
+      },
+    ) => {
+      const profile = await context.prismaClient.profile.update({
+        where: { id: args.id },
+        data: args.dto,
+      });
       return profile;
     },
   },
