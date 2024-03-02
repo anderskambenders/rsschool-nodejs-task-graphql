802fd1d5ff31f9d2f6d67d922ad3f30b822114f5
anderskambenders <a.zetsmanis@mail.ru>
2024-03-02 16:16:47 +0300

    feat: add change, delete user

--- src/routes/graphql/schemas/user/mutation.ts
+++ src/routes/graphql/schemas/user/mutation.ts
@@ -1,12 +1,21 @@
-import { GraphQLInputObjectType, GraphQLNonNull, GraphQLString, GraphQLFloat } from 'graphql';
+import { GraphQLInputObjectType, GraphQLNonNull, GraphQLString, GraphQLFloat, GraphQLBoolean } from 'graphql';
 import { userType } from './queries.js';
 import { PrismaClient } from '@prisma/client';
+import { UUIDType } from '../../types/uuid.js';
 
 export const createUserInputType = new GraphQLInputObjectType({
   name: 'CreateUserInput',
   fields: () => ({
     name: { type: new GraphQLNonNull(GraphQLString) },
     balance: { type: new GraphQLNonNull(GraphQLFloat) },
+  }),
+});
+
+export const changeUserInputType = new GraphQLInputObjectType({
+  name: 'ChangeUserInput',
+  fields: () => ({
+    name: { type: GraphQLString },
+    balance: { type: GraphQLFloat },
   }),
 });
 
@@ -25,6 +34,40 @@ export const userMutations = {
       },
     ) => {
       const user = await context.prismaClient.user.create({ data: args.dto });
+      return user;
+    },
+  },
+  deleteUser: {
+    type: GraphQLBoolean,
+    args: { id: { type: UUIDType } },
+    resolve: async (_parent: unknown, args: { id: string }, context: {
+      prismaClient: PrismaClient;
+    }) => {
+      try {
+        await context.prismaClient.user.delete({ where: { id: args.id } });
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
+  },
+  changeUser: {
+    type: userType,
+    args: { id: { type: UUIDType }, dto: { type: changeUserInputType } },
+    resolve: async (
+      _parent: unknown,
+      args: { id: string; dto: {
+        name: string;
+        balance: number;
+      } },
+      context: {
+        prismaClient: PrismaClient;
+      },
+    ) => {
+      const user = await context.prismaClient.user.update({
+        where: { id: args.id },
+        data: args.dto,
+      });
       return user;
     },
   },
